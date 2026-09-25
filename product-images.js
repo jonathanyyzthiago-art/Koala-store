8c744511145a1543e0f60a7caafdccd41ce88996
jonathanyyzthiago-art <jonathanyyz.thiago@gmail.com>
2026-09-25 17:55:16 -0300

    Delete product-images.js

--- product-images.js
+++
@@ -1,7 +0,0 @@
-/* Imagens permanentes da Koala Store. Todos os caminhos são relativos. */
-const PRODUCT_IMAGES = Object.freeze({
-  assinaturas: "products/assinaturas.jpg",
-  nitro: "products/nitro-discord.jpg",
-  texturas: "products/texturas-minecraft.jpg",
-  discordCategory: "products/discord-category.jpg"
-});
