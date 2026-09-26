5b0ac95e170f4a9c4c6e6c7234d8e6d16668a777
jonathanyyzthiago-art <jonathanyyz.thiago@gmail.com>
2026-09-25 23:28:34 -0300

    Add files via upload

---
+++ product-images.js
@@ -0,0 +1,10 @@
+/*
+ * Koala Store - imagens permanentes dos produtos.
+ * Todos os caminhos são relativos ao index.html para funcionar no GitHub Pages.
+ */
+const PRODUCT_IMAGES = {
+  assinaturas: "products/assinaturas.jpg",
+  nitro: "products/nitro-discord.jpg",
+  texturas: "products/texturas-minecraft.jpg",
+  discordCategory: "products/discord-category.jpg"
+};
